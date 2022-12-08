2a8ec3e211d4212b213e3e967a2e07083456b676
newazbenalam <newazbenalam@gmail.com>
2022-12-09 01:28:29 +0600

    janky derkmode

--- public/script.js
+++ public/script.js
@@ -2,10 +2,13 @@ function darkMode() {
   if (document.body.style.backgroundColor == "black") {
       document.body.style.backgroundColor = "white";
       // document.body.style.color = "black";
-      document.body.style.color = "white";
-      document.body.style.dev.color = "white";
+      document.getElementById("hero").style.background = "linear-gradient(0deg, var(--primary-color) 0%, var(--bg-color) 60%)";
+      document.body.style.color = "black";
+      document.body.style.dev.color = "black";
   } else {
       document.body.style.backgroundColor = "black";
+      document.getElementById("hero").style.background = "linear-gradient(0deg, #261b2e 0%, #868bb9 60%)";
+      ocument.getElementByTagName("a")[0-10].style.background = "white";
       document.body.style.color = "white";
       document.body.style.dev.color = "white";
   }
