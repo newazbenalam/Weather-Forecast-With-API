9239cea1d910e34513c28e4520f856385e320122
newazbenalam <newazbenalam@gmail.com>
2022-12-11 06:01:29 +0600

    hourly forecast

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var api_key='';
+var api_key='2399352d04a2eceb5b84ee8d5e7be2cc';
 var locat = 'Dhaka';
 
 function darkMode() {
@@ -34,33 +34,78 @@ async function foo(location) {
   let obj;
   const res = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ capitalizeFirstLetter(location) +'&appid=' + api_key + '&units=metric')
   obj = await res.json();
-  console.log((new Date()).toLocaleString("default", {hour12: false, hour: '2-digit', minute: '2-digit'}) );
-  document.getElementById('temp').textContent = parseInt(obj.main.temp);
+  let dt = new Date((obj.dt + obj.timezone)*1000);
+  console.log(dt);
+  document.getElementById('temp').textContent = parseInt(obj.main.temp) + '°C';
   document.getElementById('min_temp').textContent = parseInt(obj.main.temp_min) + '°C';
   document.getElementById('max_temp').textContent = parseInt(obj.main.feels_like + 2) + '°C';
   document.getElementById('feels_temp').textContent = parseInt(obj.main.feels_like) + '°C';
   document.getElementById('locat').textContent = obj.name;
   document.getElementById('pressure').textContent = parseInt(obj.main.pressure);
   document.getElementById('air').textContent = parseInt(obj.visibility / obj.main.humidity);
-  document.getElementById('uv_index').textContent = parseInt(obj.clouds.all) + 2;
+  document.getElementById('uv_index').textContent = parseInt(obj.clouds.all+2) % 10;
   document.getElementById('wind').textContent = (obj.wind.speed);
   document.getElementById('humidity').textContent = parseInt(obj.main.humidity) + '%';
   document.getElementById('condition').textContent = obj.weather[0].main;
   document.getElementById('visibility').textContent = obj.visibility;
   document.getElementById('clouds').textContent = parseInt(obj.clouds.all);
-  document.getElementById('weekday').textContent = (new Date()).toLocaleString("default", { weekday: "long" }) + ", ";
-  document.getElementById('stime').textContent = (new Date()).toLocaleString("default", {hour12: false, hour: '2-digit', minute: '2-digit'});
+  document.getElementById('weekday').textContent = dt.toLocaleString("default", { weekday: "long", timeZone: "UTC" }) + ", ";
+  document.getElementById('stime').textContent = dt.toLocaleString("default", {hour12: false, hour: '2-digit', minute: '2-digit', timeZone: "UTC"});
+  document.getElementById("highlight").textContent = "Today's Highlight";
 }
 // foo(locat);
+
+let castObj;
+async function fetchCast(location){
+  const res = await fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ capitalizeFirstLetter(location) +'&appid=' + api_key + '&units=metric')
+  castObj = await res.json();
+  cast();
+}
+
+function cast() {
+  let i = 0;
+  document.querySelectorAll('[id=f-temp]').forEach(element => {
+    element.textContent = (new Date((castObj.list[i+=2].dt)*1000)).toLocaleString("default", {hour12: false, hour: '2-digit', minute: '2-digit', timeZone: "UTC"});
+  });
+  i=0;
+  document.querySelectorAll('[id=f-max-temp]').forEach(element => {
+    element.textContent = parseInt(castObj.list[i+=2].main.temp_max + 1);
+  });
+  i=0;
+  document.querySelectorAll('[id=f-min-temp]').forEach(element => {
+    element.textContent = parseInt(castObj.list[i+=2].main.temp_min);
+  });
+  i=0;
+  document.querySelectorAll('[id=f-condition]').forEach(element => {
+    element.textContent = castObj.list[i+=2].weather[0].main;
+  });
+
+}
+
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+let eeel;
+document.querySelectorAll('[id=h-card]').forEach(a => a.addEventListener("click", (e)=>{
+  window.scrollTo(0, 0);
+  eeel = e;
+  document.getElementById("highlight").textContent = (e.target.textContent || e.target.parentElement.textContent) + "'s Highlight";
+}));
+
+function onclickcard(e){
+  console.log('click!')
+  eeel = e;
+  document.getElementById("highlight").textContent = e.path[0].outerText;
+}
+
 var searchbar = document.getElementById("searchbar");
 searchbar.addEventListener("keydown", function (e) {
     if (e.code === "Enter") { 
+        locat = e.target.value;
         foo(e.target.value);
+        fetchCast(locat);
     }
 });
 
@@ -73,9 +118,12 @@ function getKey() {
   // console.log(api_key);
   if (api_key == null){
     api_key = prompt("Please enter api key:");
-    localStorage.setItem('apikey', api_key);
+    if (!api_key == ''){
+      localStorage.setItem('apikey', api_key);
+    }
   }
   foo(locat);
+  fetchCast(locat);
 }
-setTimeout(() => { getKey(); }, 1000);
+// setTimeout(() => { getKey(); }, 1000);
 
